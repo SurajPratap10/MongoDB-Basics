ce1545eb9cf7743ced62c77e82230d3347d54bd6
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-03-26 01:39:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/sample").then(() => {
+    console.log("Connected to MongoDB");
+
+}).catch((errs) => {
+    console.log("error");
+})
+
+// student is the SCHEMA here
+const student = new mongoose.Schema({
+    name:String,
+    height:Number
+
+});
+
+const Student = new mongoose.model("Student", student);
+
+const adder = async () => {
+
+    //for adding in database
+
+// const ss = await Student.create({
+//     name:"Sam",
+//     height:5
+// });
+
+    // for showing datat in database
+    // const ss = await Student.find();
+
+
+    // Conditional Operators 
+// const ss = await Student.find({height:{$eq:6}});  // $ use hota and eq means equal
+    const ss = await Student.find({height:{$gte:6}}); // >=
+    console.log(ss);
+
+}
+
+adder();
+
